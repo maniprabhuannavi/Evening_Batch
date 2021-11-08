01b2a37e7a59721926c356359353d55fc9abcc91
maniprabhuannavi <maniprabhu3411@gmail.com>
2021-11-08 16:23:45 +0000

    new project

---
+++ src/com/javaconcepts/Base_Class.java
@@ -0,0 +1,5 @@
+package com.javaconcepts;
+
+public class Base_Class {
+
+}
---
+++ src/com/javaconcepts/Java_Concepts_1.java
@@ -0,0 +1,8 @@
+package com.javaconcepts;
+
+public class Java_Concepts_1 {
+	
+public static void main(String[] args) {
+	System.out.println("Testing");
+}
+}
